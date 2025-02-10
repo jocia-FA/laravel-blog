39206a25eba798bb7521a0cc22f125790af0db79
jocia-FA <felicia.pawira@gmail.com>
2025-02-10 23:22:33 +0700

    Posts & Layouts View (app, index)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index() { // get
+        $post = "Laravel Tutorial Series One!";
+        return view('posts.index', ['post'=>$post]);
+    }
 }
